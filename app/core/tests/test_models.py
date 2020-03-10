469e476a91022f8cd58f0c8b1e15e87c65913d18
Fakhar Mahmood <fakharmahmood38@gmail.com>
2020-03-10 21:58:00 +0500

    Created & Tested Tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 class ModelTest(TestCase):
     """Contains the test functions for system's models"""
+    def sample_user(self):
+        email = 'test@teamalif.com'
+        password = 'testpass@123'
+        return get_user_model().objects.create_user(email=email,
+                                                    password=password)
 
     def test_user_created_with_email(self):
         """Tests if a user is created with the email as identifier"""
@@ -37,3 +43,14 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_superuser(email, 'test123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """
+            Test the string representation of Tag model
+        """
+        tag = models.Tag.objects.create(
+            user=self.sample_user(),
+            name='Vegetable',
+        )
+
+        self.assertEqual(str(tag), tag.name)
